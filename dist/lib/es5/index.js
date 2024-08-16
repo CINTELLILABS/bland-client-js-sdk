e2322a0053963d78042f47778436c3d18ed3e74b
jumpingcat <90723496+j1llywilly@users.noreply.github.com>
2024-08-15 22:58:24 -0700

    pathways

--- dist/lib/es5/index.js
+++ dist/lib/es5/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.BlandWebClient = void 0;
+exports.PathwayUI = exports.BlandWebClient = void 0;
+var pathway_1 = require("./client/pathway");
+exports.PathwayUI = pathway_1.default;
 var BlandClient_1 = require("./client/BlandClient");
 Object.defineProperty(exports, "BlandWebClient", { enumerable: true, get: function () { return BlandClient_1.BlandWebClient; } });
---
+++ dist/lib/es6/client/pathway.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+interface PathwayUIProps {
+    apiKey: string;
+    pathwayId: string;
+    style?: React.CSSProperties;
+}
+declare const PathwayUI: React.FC<PathwayUIProps>;
+export default PathwayUI;
